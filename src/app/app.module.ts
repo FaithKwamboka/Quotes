9087456184d6ba0f2a3fb4b4a2f1adaddf13a30f
FaithKwamboka <okongofaith3@gmail.com>
2022-04-09 10:10:01 +0300

    Cretaing quote detail compenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightDirective } from './highlight.directive';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    HighlightDirective
+    HighlightDirective,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
